0e9935e3566175b821318d0dee5ea15f033e08c4
jinzhu <jinzhu@users.noreply.github.com>
2022-04-26 02:32:22 +0000

    Deploying to gh-pages

---
+++ js/code_expander.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  $("figure.highlight").on("mouseover", function() {
+    let width = $(this).width();
+    $(this).data("width", $(this).width())
+    if (width < $(this).find("table").width()) {
+      $(this).width($(".article-content").width() + $("#article-toc").width());
+    }
+  }).on("mouseout", function() {
+    $(this).width($(this).data("width"));
+  })
+}())
